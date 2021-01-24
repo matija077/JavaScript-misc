a99c32f73de82d52cd412faa3f7a6595103cc24f
Matija077 <matija.prs@gmail.com>
2021-01-24 23:23:07 +0100

    sadsd

--- module.js
+++ module.js
@@ -4,14 +4,17 @@ var module1 = {
     a: 3
 };
 let t = 34;
+var as ="a";
 
 
 //export { module1 };
 
 
 console.log("module");
+setTimeout(() => {import module2 from './module2.js'});
 
 
-import('./module2.js').then(function resolved(result) {
+
+/*import('./module2.js').then(function resolved(result) {
     console.log(result);
-})
+})*/
--- module2.js
+++ module2.js
@@ -1,6 +1,7 @@
 export default "3"
 
 console.log("adasdasd");
+setTimeout(() => {console.log(as);}, 4000);
 
 import "./tes.js";
 
