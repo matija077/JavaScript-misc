d2dccdac0c7760eda05c351dd3ef25bba3c17c7a
Matija077 <matija.prs@gmail.com>
2021-02-12 20:07:32 +0100

    generatoes and more

--- ceses.js
+++ ceses.js
@@ -55,3 +55,32 @@ return function () {
   return resolve(generator.next())
 }
   }
+
+  function isElementInViewport (el) {
+
+
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
+console.log(isElementInViewport(document.getElementById("outside")));
+
+function callback() {
+  console.log("visibile");
+}
+
+const options = {
+  root: null,
+  rootMargin: '10px',
+  threshold: 1.0
+};
+
+const observer = new IntersectionObserver(callback, options)
+observer.observe(document.getElementById("outside"));
